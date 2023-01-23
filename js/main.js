114cf0f66bf051fdeabf005fc6404ac8ce5b130c
Samri0109 <117039000+Samri0109@users.noreply.github.com>
2023-01-23 11:03:34 +0100

    Remove subscription banner when you click the X

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import stickyNavigation from './modules/sticky-navigation.js';
 import mobileNavigation from './modules/mobile-navigation.js';
-
+import subscribeButton from './modules/subscribe-button.js';
 
 mobileNavigation();
 stickyNavigation();
+subscribeButton();
