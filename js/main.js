79cf2f2ec2aeb10d384d62eadbd688ca0566d062
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-01-23 09:38:47 +0100

    added js structure for events

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import stickyNavigation from './modules/sticky-navigation.js';
 import mobileNavigation from './modules/mobile-navigation.js';
+import eventSection from './modules/events-section.js';
 
 
 mobileNavigation();
 stickyNavigation();
+eventSection();
