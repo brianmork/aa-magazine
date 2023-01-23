4abea1ee3295ab3be2fbe5b9d682d4195c3085bf
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-01-23 09:51:44 +0100

    added events array

--- js/modules/events-section.js
+++ js/modules/events-section.js
@@ -1,4 +1,69 @@
 export default function eventSection() {
 
-	
+	/**
+	 * @todo if past todays date, remove from site
+	 */
+	const events = [
+		{
+			category: 'lectures',
+			date: 'january 20 12:00-14:00',
+			title: 'Conversations: Sami Sami sacred mountains and sacrificial stones',
+		},
+		{
+			category: 'exhibitions',
+			date: 'february 5 16:00-17:00',
+			title: 'Free guided tour in Kunna Guanne Concha',
+		},
+		{
+			category: 'exhibitions',
+			date: 'feburary 12 18:00',
+			title: 'CExhibition opening: Metahaven and Susanne M.Winterling',
+		},
+		{
+			category: 'open seminars',
+			date: 'march 4 18:00-20:00',
+			title: 'Monumental love - Art historian Marit Paasche on Hannah Ryggen',
+		},
+		{
+			category: 'lectures',
+			date: 'january 20 12:00-14:00',
+			title: 'Conversations: Sami Sami sacred mountains and sacrificial stones',
+		},
+		{
+			category: 'exhibitions',
+			date: 'february 5 16:00-17:00',
+			title: 'Free guided tour in Kunna Guanne Concha',
+		},
+		{
+			category: 'exhibitions',
+			date: 'feburary 12 18:00',
+			title: 'CExhibition opening: Metahaven and Susanne M.Winterling',
+		},
+		{
+			category: 'open seminars',
+			date: 'march 4 18:00-20:00',
+			title: 'Monumental love - Art historian Marit Paasche on Hannah Ryggen',
+		},
+		{
+			category: 'lectures',
+			date: 'january 20 12:00-14:00',
+			title: 'Conversations: Sami Sami sacred mountains and sacrificial stones',
+		},
+		{
+			category: 'exhibitions',
+			date: 'february 5 16:00-17:00',
+			title: 'Free guided tour in Kunna Guanne Concha',
+		},
+		{
+			category: 'exhibitions',
+			date: 'feburary 12 18:00',
+			title: 'CExhibition opening: Metahaven and Susanne M.Winterling',
+		},
+		{
+			category: 'open seminars',
+			date: 'march 4 18:00-20:00',
+			title: 'Monumental love - Art historian Marit Paasche on Hannah Ryggen',
+		},
+	]
+
 }
