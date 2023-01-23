4ef6f06cc28ad780420e87c985c687a99498f256
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-01-23 09:28:58 +0100

    added script link and fixed some navigation bugs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-// import mobileNavigation from './modules/mobileNavigation.js';
 import stickyNavigation from './modules/sticky-navigation.js';
 import mobileNavigation from './modules/mobile-navigation.js';
 
 
-// mobileNavigation();
+mobileNavigation();
 stickyNavigation();
