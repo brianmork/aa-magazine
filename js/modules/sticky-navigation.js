a0645985fb62d79d981ccfdeb4b04bc01b7e12df
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-01-27 13:15:04 +0100

    fixed jumpy fixed nav

--- js/modules/sticky-navigation.js
+++ js/modules/sticky-navigation.js
@@ -1,17 +1,21 @@
 export default function stickyNavigation() {
 	const navbar = document.querySelector('.navigation__mobile');
-	let sticky = navbar.offsetTop;
-
+	const main = document.querySelector('main');
+	
+	let sticky = navbar.offsetTop - 20;
+	
 	if(navbar !== null && window.innerWidth < 992) {
 		window.addEventListener('scroll', handleNavbarScroll);
-		// console.log('hei');
 	}
 
-	function handleNavbarScroll() {
+	function handleNavbarScroll(event) {
 		if (window.pageYOffset >= sticky) {
 			navbar.classList.add("navigation__mobile-sticky")
+			main.classList.add('navbar-offset-margin');
 		 } else {
 			navbar.classList.remove("navigation__mobile-sticky");
+			main.classList.remove('navbar-offset-margin');
 		 }
 	}
+
 }
